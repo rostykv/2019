6dbbb8d18ecb1a4fa115d5421e5526ea39d941c3
rostykv <rostykv@gmail.com>
2019-04-17 13:50:30 +0300

    Added html template

--- lesson1/views.py
+++ lesson1/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def company_list(request):
+    return render(request, 'lesson1/company_list.html', {})
