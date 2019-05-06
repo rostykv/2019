8e094267b880e5c3c016a7a6103cfb122a712e2d
rostykv <rostykv@gmail.com>
2019-05-06 21:26:28 +0300

    Prime numbers code improved

--- lesson1/views.py
+++ lesson1/views.py
@@ -4,21 +4,18 @@ from .models import Company
 # Create your views here.
 
 def prime_numbers(max):
+    yield 1
     yield 2
-    list = [2,3]
-    while list[-1] < max:
-        for d in list:
-            prime = True
-            if (d**2>list[-1]):
+    l = [2,3]
+    while (l[-1] < max):
+        for d in l:
+            if (l[-1] % d == 0):
+                l[-1]+=2
                 break
-            if (list[-1] % d == 0):
-                prime = False
+            if (d**2 > l[-1]):
+                yield l[-1]
+                l.append(l[-1] + 2)
                 break
-        if prime:
-            yield list[-1]
-            list.append(list[-1] + 2)
-        else:
-            list[-1]+=1
     return
 
 
